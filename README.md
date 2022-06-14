1877854a9472fc5c19cc271d525a49afd47381f3
Lay <lei4519@126.com>
2022-06-14 22:30:06 +0800

    feat: linux

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 Getting cookies by reading Chrome's cookie database on your computer.
 
-Thanks to (chrome-cookies-secure)[https://github.com/bertrandom/chrome-cookies-secure], this is the rust version of it, why implement it again with Rust?
+![demo](https://raw.githubusercontent.com/lei4519/picture-bed/main/images/demo-min.gif)
+
+Thanks to [chrome-cookies-secure](https://github.com/bertrandom/chrome-cookies-secure), this is the rust version of it, why implement it again with Rust?
 1. chrome-cookies-secure depends on the Node environment. If you want to use it in Go or Python, you must install Node first.
 2. Even in the node environment, it doesn't work all the time. For example, when writing vscode extension, the `sqlite` dependency is not supported in vscode, so I had to fork the repo and change the dependency to `vscode-sqlite`.
 
@@ -23,6 +25,24 @@ extract-chrome-cookies unset-global-hook && rm /usr/local/bin/extract-chrome-coo
 ## Use
 
 ```sh
+extract-chrome-cookies https://google.com
+```
+
+- Format cookies 
+
+```sh
+extract-chrome-cookies -f curl https://google.com
+```
+
+- For more details
+
+```sh
 extract-chrome-cookies -h
 ```
+
+## FAQ
+
+Q: Why not supported windows?
+
+A: I can't find a Rust API equivalent to JS, and my knowledge of encryption/decryption is pretty poor. If you have the ability to implement them, you are welcome to submit the Pull Request
 
--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,16 @@ use std::{
 };
 use url::Url;
 
-use clap::{Arg, Command};
+use clap::{Arg, ArgMatches, Command};
 
 static SALT: &[u8; 9] = b"saltysalt";
 const KEYLENGTH: usize = 16;
 const IV: [u8; 16] = [32; KEYLENGTH];
 const MAC_ITERATIONS: u32 = 1003;
 const LINUX_ITERATIONS: u32 = 1;
-
+const FORMATS: [&str; 4] = ["curl", "header", "json", "object"];
+const FORMAT_HELP: &str =
+    "Control how cookies are formatted, One of 'curl', 'header', 'json', 'object'";
 #[derive(Debug)]
 struct Cookie {
     host_key: String,
@@ -42,9 +44,72 @@ struct Cookie {
 }
 
 fn main() {
-    let formats = ["curl", "header", "json", "object"];
+    let matchs = build_cli().get_matches();
 
-    let m = Command::new("extract-chrome-cookies")
+    run(matchs);
+}
+
+fn run(matchs: ArgMatches) {
+    let url = matchs.value_of("url").unwrap();
+    let format = matchs.value_of("format").unwrap();
+    let profile = matchs.value_of("profile_name").unwrap();
+    let profile_path = matchs.value_of("profile_path");
+
+    let url = match Url::parse(url) {
+        Ok(url) => url,
+        Err(e) => err(&format!("URL Parse Error: {}", e)),
+    };
+
+    let domain = match url.domain() {
+        Some(domain) => domain,
+        None => {
+            err("Could not parse domain from URI, format should be http://www.example.com/path/")
+        }
+    };
+
+    // println!("{:#?}", url);
+    // println!("{:#?}", format);
+    // println!("{:#?}", profile);
+    // println!("{:#?}", profile_path);
+
+    let path;
+    let mut home_path = String::new();
+    if profile_path.is_some() {
+        path = profile_path.unwrap().to_string();
+    } else {
+        home_path = match get_home_path() {
+            Some(path) => path,
+            None => cant_resolve_profile_path(None),
+        };
+
+        path = get_profile_path(&home_path, profile) + "/Cookies";
+    }
+
+    if !Path::new(&path).exists() {
+        let home = if home_path.is_empty() {
+            None
+        } else {
+            Some(home_path)
+        };
+        cant_resolve_profile_path(home);
+    }
+
+    let connection = match Connection::open(path) {
+        Ok(v) => v,
+        Err(e) => err(&format!("Open database Failed: {e}")),
+    };
+
+    let cookies = get_cookies(&connection, domain).unwrap();
+
+    let result = format_cookies(format, &cookies);
+
+    connection.close();
+
+    println!("{result}");
+}
+
+fn build_cli() -> Command<'static> {
+    Command::new("extract-chrome-cookies")
         .version("0.0.1")
         .about("Getting cookies by reading Chrome's cookie database on your computer.")
         .arg(
@@ -58,21 +123,20 @@ fn main() {
                 .takes_value(true)
                 .default_value("curl")
                 .validator(|v| {
-                    if formats.contains(&v) {
+                    if FORMATS.contains(&v) {
                         return Ok(());
                     }
-                    Err(format!("{}{:?}", "Must One of ", &formats))
+                    Err(format!("{}{:?}", "Must One of ", &FORMATS))
                 })
-                .help(&*format!(
-                    "{}{:?}",
-                    "Control how cookies are formatted, One of ", &formats
-                )),
+                .help(FORMAT_HELP),
         )
         .arg(
             Arg::new("profile_name")
                 .short('n')
                 .takes_value(true)
-                .default_value("Default")
+                // TODO
+                // .default_value("Default")
+                .default_value("Profile 1")
                 .help("Chrome Profile Name"),
         )
         .arg(
@@ -81,82 +145,6 @@ fn main() {
                 .takes_value(true)
                 .help("Direct pass chrome profile path"),
         )
-        .get_matches();
-
-    let url = m.value_of("url").unwrap();
-    let format = m.value_of("format").unwrap();
-    let profile = m.value_of("profile_name").unwrap();
-    let profile_path = m.value_of("profile_path");
-
-    let url = match Url::parse(url) {
-        Ok(url) => url,
-        Err(e) => err(&format!("URL Parse Error: {}", e)),
-    };
-
-    let domain = match url.domain() {
-        Some(domain) => domain,
-        None => {
-            err("Could not parse domain from URI, format should be http://www.example.com/path/")
-        }
-    };
-    // println!("{:#?}", url);
-    // println!("{:#?}", format);
-    // println!("{:#?}", profile);
-    // println!("{:#?}", profile_path);
-
-    let home_path = match get_home_path() {
-        Some(path) => path,
-        None => cant_resolve_profile_path(None),
-    };
-
-    let path = get_profile_path(&home_path, profile) + "/Cookies";
-
-    if !Path::new(&path).exists() {
-        cant_resolve_profile_path(Some(&home_path));
-    }
-
-    let connection = match Connection::open(path) {
-        Ok(v) => v,
-        Err(e) => err(&format!("Open database Failed: {e}")),
-    };
-
-    let cookies = get_cookies(&connection, domain).unwrap();
-
-    let result = format_cookies(format, &cookies);
-
-    println!("{result}");
-}
-
-fn get_derived_key() -> Option<[u8; 16]> {
-    match env::consts::OS {
-        "macos" => {
-            // format!("{home_path}/Library/Application Support/Google/Chrome/{profile}",)
-            if let Ok(res) = keytar::get_password("Chrome Safe Storage", "Chrome") {
-                if res.success {
-                    let mut buffer: [u8; 16] = [0; 16];
-                    let mut m = Hmac::new(Sha1::new(), res.password.as_bytes());
-                    pbkdf2(&mut m, SALT, MAC_ITERATIONS, &mut buffer);
-
-                    return Some(buffer);
-                }
-            }
-            return None;
-        }
-        // TODO other platform
-        // "linux" => {
-        // crypto.pbkdf2('peanuts', SALT, ITERATIONS, KEYLENGTH, 'sha1', callback);
-        // }
-        // "windows" => {
-        //     let mut p = format!(
-        //         "{home_path}\\AppData\\Local\\Google\\Chrome\\User Data\\{profile}\\Network"
-        //     );
-        //     if !Path::new(&p).exists() {
-        //         p = format!("{home_path}\\AppData\\Local\\Google\\Chrome\\User Data\\{profile}");
-        //     }
-        //     p
-        // }
-        _ => None,
-    }
 }
 
 fn err(msg: &str) -> ! {
@@ -164,7 +152,7 @@ fn err(msg: &str) -> ! {
     exit(1);
 }
 
-fn cant_resolve_profile_path(home_path: Option<&str>) -> ! {
+fn cant_resolve_profile_path(home_path: Option<String>) -> ! {
     if home_path.is_none() {
         print!("{}\n\n", "The program can't resolve your Google profile path automatically, Please use '-p' option to pass it in manually.".red());
     } else {
@@ -176,7 +164,7 @@ fn cant_resolve_profile_path(home_path: Option<&str>) -> ! {
     print!("  {}\n\n", "3. Find 'profile path'");
 
     if home_path.is_some() {
-        let path = get_profile_path(home_path.unwrap(), "<PROFILE NAME>");
+        let path = get_profile_path(&home_path.unwrap(), "<PROFILE NAME>");
         print!("{}\n", "If the path looks like this:".green());
         print!("  {}\n\n", path);
         print!("{}\n", "You just pass in the profile name:".green());
@@ -229,6 +217,7 @@ fn get_profile_path(home_path: &str, profile: &str) -> String {
 }
 
 fn get_cookies(conn: &Connection, domain: &str) -> Result<Vec<Cookie>, rusqlite::Error> {
+    // only macos linux can go here, so value must a Some
     let key = get_derived_key().unwrap();
 
     let statement = format!("SELECT host_key, path, is_secure, expires_utc, name, value, encrypted_value, creation_utc, is_httponly, has_expires, is_persistent FROM cookies where host_key like '%{domain}' ORDER BY LENGTH(path) DESC, creation_utc ASC");
@@ -266,11 +255,52 @@ fn get_cookies(conn: &Connection, domain: &str) -> Result<Vec<Cookie>, rusqlite:
 
         if cookie.value.is_empty() && !cookie.encrypted_value.is_empty() {
             cookie.value = decrypt(&key, &cookie.encrypted_value);
+            // if (process.platform === 'win32') {
+            //   if (encryptedValue[0] == 0x01 && encryptedValue[1] == 0x00 && encryptedValue[2] == 0x00 && encryptedValue[3] == 0x00) {
+            //     cookie.value = dpapi.unprotectData(encryptedValue, null, 'CurrentUser').toString('utf-8');
+
+            //   } else if (encryptedValue[0] == 0x76 && encryptedValue[1] == 0x31 && encryptedValue[2] == 0x30) {
+            //     localState = JSON.parse(fs.readFileSync(os.homedir() + '/AppData/Local/Google/Chrome/User Data/Local State'));
+            //     b64encodedKey = localState.os_crypt.encrypted_key;
+            //     encryptedKey = new Buffer.from(b64encodedKey, 'base64');
+            //     key = dpapi.unprotectData(encryptedKey.slice(5, encryptedKey.length), null, 'CurrentUser');
+            //     nonce = encryptedValue.slice(3, 15);
+            //     tag = encryptedValue.slice(encryptedValue.length - 16, encryptedValue.length);
+            //     encryptedValue = encryptedValue.slice(15, encryptedValue.length - 16);
+            //     cookie.value = decryptAES256GCM(key, encryptedValue, nonce, tag).toString('utf-8');
+            //   }
+            // }
         }
 
         cookies.push(cookie);
     }
     Ok(cookies)
+}
+
+fn get_derived_key() -> Option<[u8; 16]> {
+    match env::consts::OS {
+        "macos" => {
+            // format!("{home_path}/Library/Application Support/Google/Chrome/{profile}",)
+            if let Ok(res) = keytar::get_password("Chrome Safe Storage", "Chrome") {
+                if res.success {
+                    let mut buffer: [u8; 16] = [0; 16];
+                    let mut m = Hmac::new(Sha1::new(), res.password.as_bytes());
+                    pbkdf2(&mut m, SALT, MAC_ITERATIONS, &mut buffer);
+
+                    return Some(buffer);
+                }
+            }
+            return None;
+        }
+        "linux" => {
+            let mut buffer: [u8; 16] = [0; 16];
+            let mut m = Hmac::new(Sha1::new(), b"peanuts");
+            pbkdf2(&mut m, SALT, MAC_ITERATIONS, &mut buffer);
+
+            return Some(buffer);
+        }
+        _ => None,
+    }
 }
 
 fn decrypt(key: &[u8], encrypted_data: &[u8]) -> String {
